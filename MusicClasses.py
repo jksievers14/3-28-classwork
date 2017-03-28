b8e2e438a8838c736ff2e3d7e9377363f310de8a
jksievers14 <jksievers14@gmail.com>
2017-03-28 14:55:37 -0400

    Create MusicClasses.py

---
+++ MusicClasses.py
@@ -0,0 +1,22 @@
+3-28-classwork
+# use classes to manage multiple data attributes
+
+class Music():
+    # this sets up the variables in the object
+    def __init_(self):
+        self.platform=''
+        self.artist=''
+        self.genre=''
+        self.age=''
+        self.sex=''
+        self.albums=''
+
+# create an instance of this class
+ipod = Music()
+
+ipod.platform = 'iTunes'
+ipod.genre = 'Hip Hop'
+ipod.artist = 'Travis Scott'
+ipod.age='24'
+ipod.sex='Male'
+ipod.albums= '2'
